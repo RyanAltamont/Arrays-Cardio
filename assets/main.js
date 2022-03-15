fdb7caaabaa4c37ad1dd4ec149a41db80af0a2ed
RyanAltamont <97465962+RyanAltamont@users.noreply.github.com>
2022-03-15 08:21:04 -0500

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,55 @@
+// 1. Create an array - with a name that makes sense - that contains the following items: English, History, Science, PE, Math, Computer Science, World Language. Write a second statement that logs this array to the console.
+  const courses = [`English`, `History`, `Science`, `PE`, `Math`, `Computer Science`, `World Language`,];
+
+// 2. Using an array method, add an item to the end of the array for Fine Arts. Log the updated array to the console.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+courses.push(`Fine Arts`);
+console.log(courses);
+// 3. Using an array sorting method, arrange the array in alphabetical order and log it to the console.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+  courses.sort();
+  console.log(courses);
+// 4. Using bracket notation, output a statement to the console that says something like, `My favorite class is ___________!`
+  console.log(`My favorite class is ${`Math`}!`);
+// 5. Using an array method, output a statement to the console that lets a user know what the first three classes are.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+  courses.slice ();
+  console.log(courses.slice (0,3));
+
+
+// 6. Using an array method, remove the last class from the array and set it equal to a new variable. Then, log the updated version to the console.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+const popped = courses.pop ();
+console.log(courses);
+
+
+// 7. Using an array method, add the removed class back and print the updated version to the console.
+
+courses.push (`World Language`);
+console.log(courses);
+
+
+
+
+// 8. Using dot notation, write a statement that outputs a sentence saying how many items are in the array.
+// Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length
+  console.log(`There are ${courses.length} items in the array.`);
+
+
+
+var grades = [89, 91, 78, 98, 82, 83, 98, 100]; // Use this array for the remaining questions
+
+// 9. Using bracket notation, construct a statement that is true comparing two values in the array using the `==` operators.
+
+89 == [0];
+console.log(89 == [89]);
+
+
+
+
+
+
+// 10. Using bracket notation, construct a statement that is false comparing two values in the array using the `>=` operators.
+
+89 >= [1];
+console.log(89 >= [91]);
